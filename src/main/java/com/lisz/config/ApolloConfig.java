c06c786f0ec7e3de6ebf07de3346b025adb87513
lisz1012 <lisz1012@163.com>
2021-05-01 01:57:16 -0700

    Add secret keys and grayscale release

--- src/main/java/com/lisz/config/ApolloConfig.java
+++ src/main/java/com/lisz/config/ApolloConfig.java
@@ -1,4 +1,24 @@
 package com.lisz.config;
 
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigService;
+import com.ctrip.framework.apollo.model.ConfigChange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ApolloConfig {
+
+	// Add more logic when the changes happened in the config service
+	@Bean
+	public void config(){
+		Config config = ConfigService.getAppConfig();
+		config.addChangeListener(event -> {
+			System.out.println("Changes for namespace: " + event.getNamespace());
+			for (String key : event.changedKeys()){
+				ConfigChange change = event.getChange(key);
+				System.out.println(String.format("Found changes: key [%s] old value [%s] new value [%s]", key, change.getOldValue(), change.getNewValue()));
+			}
+		});
+	}
 }
--- src/main/java/com/lisz/controller/MainController.java
+++ src/main/java/com/lisz/controller/MainController.java
@@ -8,10 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 	@Value("${DBName}")
 	private String name;
+	@Value("${Redis}")
+	private String redis;
 
 	@GetMapping("/")
 	public String list(){
 		System.out.println(name);
 		return "11" + name;
+	}
+	@GetMapping("/redis")
+	public String redis(){
+		System.out.println(redis);
+		return "11" + redis;
 	}
 }
